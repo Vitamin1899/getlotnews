a275f62b6ad8ae68c71322a69b50462337750a0d
Vitamin1899 <vitalimarinich@gmail.com>
2016-01-21 23:32:13 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Getlotnews::Application.config.secret_key_base = '5c03df30802fda63581004ff14ba145a410abfcaf52f2d916d79490edc87c1b46151c951598a6d0af1d2d080ea9001204a06c0db03fd09e9f458d9a21d99e34a'
